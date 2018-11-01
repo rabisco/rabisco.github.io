ee878b85eb8c9155d568ca5d8a78b1db5c16fe06
Matheus Lessa <matheus@fastmail.com>
2018-11-01 17:13:29 -0300

    separando o undo

--- main.js
+++ main.js
@@ -9,10 +9,6 @@ const erase_width = 50.0;
 
 const font_style = "#444";
 
-const undo_stack = [];
-let current_undo_index = -1;
-let current_undo_lines = [];
-
 let prev_x = 0.0;
 let prev_y = 0.0;
 let curr_x = 0.0;
@@ -93,38 +89,6 @@ function clear_screen_if_first_action() {
 	}
 }
 
-function add_undo_line(x0, y0, x1, y1) {
-	current_undo_lines.push({ x0: x0, y0: y0, x1: x1, y1: y1 });
-}
-
-function add_undo_command(is_drawing) {
-	undo_stack.push({
-		is_drawing: is_drawing,
-		lines: current_undo_lines,
-		index: current_undo_index,
-	});
-	current_undo_index += 1;
-	current_undo_lines = [];
-}
-
-function draw_undo_state_at(index) {
-	context.fillStyle = erase_style;
-	context.fillRect(0, 0, canvas.width, canvas.height);
-
-	for (i = 0; i <= index; i++) {
-		c = undo_stack[i];
-		if (c.is_drawing) {
-			for (l of c.lines) {
-				api.draw(l.x0, l.y0, l.x1, l.y1);
-			}
-		} else {
-			for (l of c.lines) {
-				api.erase(l.x0, l.y0, l.x1, l.y1);
-			}
-		}
-	}
-}
-
 const api = {
 	enabled: true,
 	draw: function (x0, y0, x1, y1) {
@@ -165,7 +129,6 @@ const api = {
 		context.fillText(extra_info, canvas.width * 0.5, canvas.height * 0.5 + 200);
 	},
 	finish_command: function () {
-		undo_stack.splice(current_undo_index + 1);
 		add_undo_command(is_drawing, current_undo_lines);
 	},
 	undo: function () {
---
+++ undo.js
@@ -0,0 +1,37 @@
+const undo_stack = [];
+let current_undo_index = -1;
+let current_undo_lines = [];
+
+function add_undo_line(x0, y0, x1, y1) {
+	current_undo_lines.push({ x0: x0, y0: y0, x1: x1, y1: y1 });
+}
+
+function add_undo_command(is_drawing) {
+	undo_stack.splice(current_undo_index + 1);
+
+	undo_stack.push({
+		is_drawing: is_drawing,
+		lines: current_undo_lines,
+		index: current_undo_index,
+	});
+	current_undo_index += 1;
+	current_undo_lines = [];
+}
+
+function draw_undo_state_at(index) {
+	context.fillStyle = erase_style;
+	context.fillRect(0, 0, canvas.width, canvas.height);
+
+	for (i = 0; i <= index; i++) {
+		c = undo_stack[i];
+		if (c.is_drawing) {
+			for (l of c.lines) {
+				api.draw(l.x0, l.y0, l.x1, l.y1);
+			}
+		} else {
+			for (l of c.lines) {
+				api.erase(l.x0, l.y0, l.x1, l.y1);
+			}
+		}
+	}
+}
