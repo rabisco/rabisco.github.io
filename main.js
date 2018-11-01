8f5ece40d5d1bc1696bc933a633fea0e21ca804e
Matheus Lessa <matheus@fastmail.com>
2018-11-01 01:41:03 -0300

    remove ctrl+z message

--- main.js
+++ main.js
@@ -123,7 +123,7 @@ const api = {
 		context.fillText("left mouse button: sketch", canvas.width * 0.5, canvas.height * 0.5 - 60);
 		context.fillText("other mouse buttons: eraser", canvas.width * 0.5, canvas.height * 0.5 - 20);
 		context.fillText("f5: clear", canvas.width * 0.5, canvas.height * 0.5 + 20);
-		context.fillText("ctrl+z: undo once", canvas.width * 0.5, canvas.height * 0.5 + 60);
+		//context.fillText("ctrl+z: undo once", canvas.width * 0.5, canvas.height * 0.5 + 60);
 
 		context.fillText(extra_info, canvas.width * 0.5, canvas.height * 0.5 + 180);
 	},
